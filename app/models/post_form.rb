c657e8f3e0f12cd0074075954ec78028de9b6e4e
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-16 20:28:15 +0900

    バリデーションの処理の記述

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -1,6 +1,11 @@
 class PostForm
   include ActiveModel::Model
 
-  # PostFormクラスのオブジェクトがPostモデルの属性が扱えるようにする
+  # PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
   attr_accessor :text, :image
+
+  with_options presence: true do
+    validates :text
+    validates :image
+  end
 end
