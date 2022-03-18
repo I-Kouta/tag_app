28f6c67b0b0a5881751a1392df2e2349039b663d
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-18 20:31:20 +0900

    searchアクションへのリクエストの定義

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputElement = document.getElementById("post_form_tag_name");
     inputElement.addEventListener("input", () => {
       const keyword = document.getElementById("post_form_tag_name").value;
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/search/?keyword=${keyword}`, true);
     });
   };
 });
